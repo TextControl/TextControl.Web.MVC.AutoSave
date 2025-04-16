5bc7644e11a443cca75c66451da8ff94de2cf633
Zuzanna Kwiatkowska <zuzanna.kwiatkowska@textcontrol.com>
2025-04-16 10:58:41 +0200

    Updated to TX33

--- tx_mvc_autosave/Global.asax.cs
+++ tx_mvc_autosave/Global.asax.cs
@@ -2,11 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Mvc;
-using System.Web.Routing;
-using System.Web.Security;
-using System.Web.SessionState;
 using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
 
 namespace tx_mvc_autosave
 {
@@ -14,10 +13,9 @@ namespace tx_mvc_autosave
     {
         void Application_Start(object sender, EventArgs e)
         {
-            // Code that runs on application startup
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);            
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
     }
 }
